59ecf7381852ed7534f1967bd72c88ecfd0f4232
mmarinova3 <mmarinova1312@gmail.com>
2024-01-03 19:42:18 +0200

    Add Test classes

---
+++ target/test/com/winery/service/BottledWineServiceTest.java
@@ -0,0 +1,42 @@
+package com.winery.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BottledWineServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ target/test/com/winery/service/UserServiceTest.java
@@ -0,0 +1,47 @@
+package com.winery.service;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void validateAndGetUser() {
+
+    }
+
+    @Test
+    void getById() {
+
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+
+    }
+
+    @Test
+    void delete() {
+    }
+}
